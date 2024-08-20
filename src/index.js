a8df402ac274f53968ddad04dae057d309d18ceb
amarjeetk4u <github4aj@gmail.com>
2024-08-20 15:48:43 +0530

    start the project

---
+++ src/index.js
@@ -0,0 +1,70 @@
+
+//require("dotenv").config()
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js"
+
+dotenv.config({
+    path: "./.env"
+})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MONGODB connection failed !!!", error);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+
+// Using IIFE function ()() connecting data bace using try catch
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) =>{
+            console.log("ERROR", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR", error)
+        throw err
+    }
+})()
+
+*/
